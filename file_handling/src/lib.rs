307175273f9974282f82e8efc2dd748bb3d6d6c4
nschainblatt <nschainblatt@gmail.com>
2024-02-07 21:44:39 -0500

    Add function to open or create a file in append mode. Create corresponding unit test. Refactor existing unit tests

--- file_handling/src/lib.rs
+++ file_handling/src/lib.rs
@@ -1,9 +1,14 @@
 use std::fs;
+use std::fs::OpenOptions;
 use std::io;
 use std::io::Write;
 
 pub fn create_writable_file(file_path: &str) -> Result<fs::File, io::Error> {
     fs::File::create(file_path)
+}
+
+pub fn open_file_or_create(file_path: &str) -> Result<fs::File, io::Error> {
+    OpenOptions::new().create(true).append(true).open(file_path)
 }
 
 pub fn write_to_file(mut file: &fs::File, contents: &[u8]) -> Result<(), io::Error> {
@@ -21,19 +26,32 @@ mod tests {
     use super::*;
 
     #[test]
-    fn create_file() {
-        create_writable_file("test-create.log").unwrap();
+    fn create_file() -> Result<(), io::Error> {
+        create_writable_file("test_create_file.log")?;
+        Ok(())
     }
 
     #[test]
-    fn write_file() {
-        let mut file = create_writable_file("test-write.log").unwrap();
-        write_to_file(&mut file, b"test123").unwrap();
+    fn write_file() -> Result<(), io::Error> {
+        let mut file = create_writable_file("test_write_file.log")?;
+        write_to_file(
+            &mut file,
+            b"This should create and write to a new file or overwrite an existing files contents",
+        )?;
+        Ok(())
     }
 
     #[test]
-    fn read_file() {
-        create_writable_file("test-read.log").unwrap();
-        read_file_contents("test-read.log").unwrap();
+    fn read_file() -> Result<(), io::Error> {
+        create_writable_file("test_read_file.log")?;
+        read_file_contents("test_read_file.log")?;
+        Ok(())
+    }
+
+    #[test]
+    fn open_or_create_file_and_write() -> Result<(), io::Error> {
+        let mut file = open_file_or_create("test_open_or_create_file_and_write.log").unwrap();
+        file.write(b"This should append text\n")?;
+        Ok(())
     }
 }
