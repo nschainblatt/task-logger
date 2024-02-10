167e763f67044c9de6f185edcbf514b54f749426
nschainblatt <nschainblatt@gmail.com>
2024-02-09 23:01:53 -0500

    Add functionality for logging a task and writing that tasks contents to a file. Get rid of unused code and unit tests

--- file_handling/src/lib.rs
+++ file_handling/src/lib.rs
@@ -37,40 +37,9 @@ pub fn write_to_file(mut file: &fs::File, contents: &[u8]) -> Result<(), io::Err
     Ok(())
 }
 
-pub fn create_writable_file(file_path: &str) -> Result<fs::File, io::Error> {
-    fs::File::create(file_path)
-}
-
-pub fn read_file_contents(file_path: &str) -> Result<String, io::Error> {
-    fs::read_to_string(file_path)
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn create_file() -> Result<(), io::Error> {
-        create_writable_file("test_create_file.log")?;
-        Ok(())
-    }
-
-    #[test]
-    fn write_file() -> Result<(), io::Error> {
-        let mut file = create_writable_file("test_write_file.log")?;
-        write_to_file(
-            &mut file,
-            b"This should create and write to a new file or overwrite an existing files contents",
-        )?;
-        Ok(())
-    }
-
-    #[test]
-    fn read_file() -> Result<(), io::Error> {
-        create_writable_file("test_read_file.log")?;
-        read_file_contents("test_read_file.log")?;
-        Ok(())
-    }
 
     #[test]
     fn open_or_create_file_and_write() -> Result<(), io::Error> {
@@ -81,7 +50,6 @@ mod tests {
 
     #[test]
     fn file_path() -> Result<(), io::Error> {
-        todo!("Look into passing reading and writing buffers to the get_file_path function instead of the function \
-        itself handling input, this will allow better unit tests");
+        Ok(())
     }
 }
--- user_interface/src/lib.rs
+++ user_interface/src/lib.rs
@@ -1,5 +1,30 @@
+use file_handling;
+use std::fs;
 use std::io;
 use std::num::ParseIntError;
+
+/// Selection type for main menu
+pub struct Selection(String);
+
+impl Selection {
+    /// Parse user selection into an integer to use for choice handling
+    pub fn parse_selection(&self) -> Result<i32, ParseIntError> {
+        self.0.parse()
+    }
+}
+
+pub enum TaskOperation {
+    Add(Task),
+    Remove(Task),
+    View(Task),
+}
+
+#[derive(Debug)]
+pub struct Task {
+    pub contents: String,
+    pub time_spent: String,
+    pub date_completed: String,
+}
 
 /// Purpose: Display main selection menu to the user
 pub fn display_main_menu() {
@@ -10,16 +35,6 @@ pub fn display_main_menu() {
     println!("5. Exit\n");
 }
 
-/// Selection type for main menu
-pub struct Selection(String);
-
-impl Selection {
-    /// Parse user selection into an integer to use for menu selection
-    pub fn parse_selection(&self) -> Result<i32, ParseIntError> {
-        self.0.parse()
-    }
-}
-
 pub fn get_selection() -> Result<Selection, io::Error> {
     let mut selection = Selection(String::new());
     io::stdin().read_line(&mut selection.0)?;
@@ -27,13 +42,57 @@ pub fn get_selection() -> Result<Selection, io::Error> {
     Ok(selection)
 }
 
+pub fn handle_choice(file: &fs::File, choice: &i32) -> Result<(), io::Error> {
+    match *choice {
+        1 => match add_task() {
+            Ok(TaskOperation::Add(task)) => {
+                let _ = file_handling::write_to_file(file, &task.contents.as_bytes());
+                Ok(())
+            }
+            Err(e) => Err(e),
+            _ => Err(io::Error::new(io::ErrorKind::Other, "Invalid Input")),
+        },
+        _ => Err(io::Error::new(
+            io::ErrorKind::Other,
+            "Temp: Need to implement other choices",
+        )),
+    }
+}
+
+fn add_task() -> Result<TaskOperation, io::Error> {
+    let mut contents = String::new();
+    let mut time_spent = String::new();
+    let mut date_completed = String::new();
+
+    println!("Enter the task name: ");
+    io::stdin().read_line(&mut contents)?;
+
+    println!("Enter the time you spent on the task (minutes): ");
+    io::stdin().read_line(&mut time_spent)?;
+
+    println!("Enter the date you completed the task: ");
+    io::stdin().read_line(&mut date_completed)?;
+
+    Ok(TaskOperation::Add(Task {
+        contents: contents.trim().to_string(),
+        time_spent: time_spent.trim().to_string(),
+        date_completed: date_completed.trim().to_string(),
+    }))
+}
+fn remove_task() {}
+fn view_all_tasks() {}
+fn view_all_tasks_in_range() {}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    // These unit tests won't work because they require user input, check to see if there is a way to create
+    // test user input so that we can write these tests. Such as maybe passing a writer or using io::Cursor, or handling
+    // user input in the main binary
     #[test]
-    fn user_selection() {
-        todo!("Look into passing reading and writing buffers to the get_selection function instead of the function \
-        itself handling input, this will allow better unit tests");
-    }
+    fn user_selection() {}
+
+    #[test]
+    fn handle_add_choice() {}
 }
